d5f12d9e376bc125f06e42e022894c815bd63016
KhalidFawziBayuomi <130516956+KhalidFawziBayuomi@users.noreply.github.com>
2024-04-30 16:37:29 +0300

    Add files via upload

--- src/bootstrab.js
+++ src/bootstrab.js
@@ -1,10 +1,12 @@
 import authRouter from "./auth/auth.routes.js"
 import userRouter from "./modules/user/user.routes.js"
+import toolsRouter from "./products/product.routs.js"
 
 
 
 export function bootstrap(app){
     app.use('/api/v1/users',userRouter)
     app.use('/api/v1/auth',authRouter)
+    app.use('/api/v1/product',toolsRouter)
     app.get('/',(req,res)=>res.send('hello world'))
 }
